14592abd3a4ca38703b768cd7913954436743887
zhangsan <zhangsan@itcast.cn>
2017-10-05 09:09:58 +0800

    项目初始化v1.0.0版本

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+    header('content-Type:text/html;charset:utf-8');
+    //后端路由
+    //默认目录名称
+    $dir='main';
+    //默认文件名称
+    $filename='index';
+    //处理url路径
+    if(array_key_exists('PATH_INFO',$_SERVER)){
+        //Path_info属性存在
+        //获取请求路径
+        $path=$_SERVER['PATH_INFO'];
+        //去掉第一个斜杠
+        $str=substr($path,1);
+        //字符串分割，和js中split方法很像
+        $ret=explode('/',$str);
+        if(count($ret)==2){
+            //路由有两层
+            $dir=$ret[0];//覆盖目标
+            $filename=$ret[1];//覆盖文件名称
+        }else{
+            //其他情况全部跳转到登录页面
+            $filename='login';
+        }
+    }
+    //嵌入子页面
+    include('./views/'.$dir.'/'.$filename.'.html');
+
+?>
